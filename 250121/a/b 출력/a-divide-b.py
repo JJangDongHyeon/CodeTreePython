556bd13b0860468c6edb4f11f06479b79382a07f
JJangDongHyeon <163490360+JJangDongHyeon@users.noreply.github.com>
2025-01-21 20:55:37 +0900

    Title: a/b 출력, Time: 41ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 250121/a/b 출력/a-divide-b.py
+++ 250121/a/b 출력/a-divide-b.py
@@ -1,9 +1,13 @@
 arr = input().split()
 
 a, b = int(arr[0]), int(arr[1])
-c = '0.'
 
 
+if a // b == 0:
+    c = '0.'
+else:
+    c = str(a // b) + '0.'
+    
 for _ in range(20):
     a = a * 10
     c += str(a // b)
