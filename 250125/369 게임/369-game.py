8eecd56e3291b2e67043b3bc2dc8829303976aac
JJangDongHyeon <163490360+JJangDongHyeon@users.noreply.github.com>
2025-01-25 12:33:43 +0900

    Title: 369 게임, Time: 47ms, Memory: 16MB, Status: Passed - CodeTree

--- 250125/369 게임/369-game.py
+++ 250125/369 게임/369-game.py
@@ -3,7 +3,9 @@ n = int(input())
 for i in range(1, n + 1):
     if i % 3 == 0:
         print(0, end = ' ')
-    elif '3' in str(i) or '6' in str(i) or '9' in str(i):
+    elif i % 10 == 3 or i % 10 == 6 or i % 10 == 9:
+        print(0, end = ' ')
+    elif i // 10 == 3 or i // 10 == 6 or i // 10 == 9:
         print(0, end = ' ')
     else:
         print(i, end =' ')
