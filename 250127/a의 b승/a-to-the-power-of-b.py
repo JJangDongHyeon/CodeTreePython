bc119ca40819a56e40972bafe51f1e80447f368b
JJangDongHyeon <163490360+JJangDongHyeon@users.noreply.github.com>
2025-01-27 12:52:13 +0900

    Title: a의 b승, Time: 45ms, Memory: 16MB, Status: Passed - CodeTree

--- 250127/a의 b승/a-to-the-power-of-b.py
+++ 250127/a의 b승/a-to-the-power-of-b.py
@@ -1,7 +1,8 @@
 arr = input().split()
 a, b = int(arr[0]), int(arr[1])
-result = a
-for i in range(1, b):
-    result = a * result
-
+result = 1
+if b != 0:
+    result = a
+    for i in range(1, b): 
+        result = a * result
 print(result)
