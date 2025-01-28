b08388b293138efa7294c2b84fad2e5e007b0682
JJangDongHyeon <163490360+JJangDongHyeon@users.noreply.github.com>
2025-01-28 13:36:10 +0900

    Title: 몫 출력하기, Time: 42ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 250128/몫 출력하기/print-share.py
@@ -0,0 +1,21 @@
+while True:
+    n = int(input())
+    if n % 2 != 0:
+        continue
+    else:
+        print(n // 2)
+        break
+while True:
+    n = int(input())
+    if n % 2 != 0:
+        continue
+    else:
+        print(n // 2)
+        break
+while True:
+    n = int(input())
+    if n % 2 != 0:
+        continue
+    else:
+        print(n // 2)
+        break
