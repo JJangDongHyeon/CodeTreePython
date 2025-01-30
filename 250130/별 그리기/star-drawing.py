bc16f15aa3117719045951c4c00a714192f317ec
JJangDongHyeon <163490360+JJangDongHyeon@users.noreply.github.com>
2025-01-30 14:36:17 +0900

    Title: 별 그리기, Time: 73ms, Memory: 18MB, Status: Passed - CodeTree

--- 250130/별 그리기/star-drawing.py
+++ 250130/별 그리기/star-drawing.py
@@ -12,6 +12,6 @@ for i in range(n):
 for i in range(n - 1):
     for j in range(i + 1):
         print(' ', end = '')
-    for j in range(n - (i * 2)):
+    for j in range((n * 2)- (i * 2) - 3):
         print('*', end = '')
     print()
