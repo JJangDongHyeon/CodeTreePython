11dd9ff146e37514cbfd49d58cd249a49e821270
JJangDongHyeon <163490360+JJangDongHyeon@users.noreply.github.com>
2025-01-30 20:45:20 +0900

    Title: 행에 따라 다르게 출력, Time: 73ms, Memory: 19MB, Status: Passed - CodeTree

--- 250130/행에 따라 다르게 출력/output-defferently-each-row.py
+++ 250130/행에 따라 다르게 출력/output-defferently-each-row.py
@@ -1,18 +1,13 @@
 n = int(input())
 
+cnt = 0
+
 for i in range(n):
-    if i % 2 == 0:
-        if i == 0:
-            for j in range(n):
-                print((n * i) + 1 + j, end = ' ')
+    for j in range(n):
+        if i % 2 == 0:
+            cnt += 1
+            print(cnt, end = ' ')
         else:
-            for j in range(n):
-                print((i + 1) * n + j + 1, end = ' ')
-    elif i % 2 != 0:
-        if i == 1:
-            for j in range(n):
-                print(n * i + (2 * (j + 1)), end = ' ')
-        else:
-            for j in range(n):
-                print((n * (i + 1)) + 2 + (j * 2), end = ' ' )
+            cnt += 2
+            print(cnt, end = ' ')
     print()
