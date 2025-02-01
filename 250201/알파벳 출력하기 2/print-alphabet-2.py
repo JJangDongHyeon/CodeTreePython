e11c6481dfa798618446ed7937f8f9e313634942
JJangDongHyeon <163490360+JJangDongHyeon@users.noreply.github.com>
2025-02-01 17:48:45 +0900

    Title: 알파벳 출력하기 2, Time: 43ms, Memory: 16MB, Status: Passed - CodeTree

--- 250201/알파벳 출력하기 2/print-alphabet-2.py
+++ 250201/알파벳 출력하기 2/print-alphabet-2.py
@@ -3,11 +3,12 @@ n = int(input())
 cnt = 65
 
 for i in range(n):
-    if cnt > 90:
-        cnt = 65
+    
     for j in range(i):
         print(' ', end = ' ')
     for j in range(n - i):
         print(chr(cnt), end = ' ')
         cnt += 1
+        if cnt > 90:
+            cnt = 65
     print()
